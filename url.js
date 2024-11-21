b83eeeda16f402eda61ff4c6734cbf9c26920d21
asagera <jamie567822567@gmail.com>
2024-11-22 00:58:34 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdnxZLxKaFNaDX51uzSrx5KquaPLi11-hTCF_hic9886_xgug/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSciv5WDasJAjZfGeiXkDZWqbWnhvlNZtr1bWDQ1xqrjalV0dg/viewform',
 ];
